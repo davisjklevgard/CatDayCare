e344ec4cf0caf7750bd8502d5aea7bc722e22a79
davisklevgard <davisjklevgard@gmail.com>
2020-12-27 18:03:24 -0600

    putting in a new class

---
+++ Clients.java
@@ -0,0 +1,13 @@
+public class Clients {
+
+	String clientName = "";
+	int numberOfCats;
+	
+
+	public void becomeClient(String name){
+		this.clientName = name;
+		System.out.println("Insert client name: " + name);
+	}
+
+	
+}
