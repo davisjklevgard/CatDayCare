d02147aec2ce0a13892061b236afebb63c9e02b3
davisklevgard <davisjklevgard@gmail.com>
2020-12-27 20:57:32 -0600

    try

--- Clients.java
+++ Clients.java
@@ -1,13 +1,32 @@
 public class Clients {
 
-	String clientName = "";
+	String clientName;
 	int numberOfCats;
+	int age;
+	int bill;
+	String favoriteFood;
 	
-
+	
+	//become a client
 	public void becomeClient(String name){
 		this.clientName = name;
 		System.out.println("Insert client name: " + name);
 	}
-
-	
+	//adding a cat
+	public void Cat (String name, String ownerName, int age, String favoriteFood){
+	this.name = name;
+	this.clientName = clientName;
+	this.age = age;
+	this.favoriteFood = favoriteFood;
+	}
+	//drop off cat
+	public void arrives(){
+		System.out.println(name + " has been dropped off.");
+	}
+	//pay bill
+	public void payBill(String name, int bill){
+		this.bill = bill;
+		this.clientName = name;
+		System.out.println(name + " has paid the bill of" + bill);
+	}
 }
